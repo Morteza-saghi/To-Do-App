d8b4d36a2e106bf83848e318b11c7adf8d3d90f3
Mo <mort.saghi@gmail.com>
2023-06-18 11:54:32 +0330

    did some of the functions of the to do app

---
+++ new.py
@@ -0,0 +1,36 @@
+class note:
+    allnotes = list()
+
+    def addnote(self, thenote):
+        note.allnotes.append(thenote)
+
+    def showallnotes(self):
+        num = 0
+        for i in note.allnotes:
+            print(num, f"{i} \n")
+            num += 1
+
+    def delNote(self):
+        note.showallnotes(self)
+        a = int(
+            input("chose the note you wnat to delete\nEnter the Index of the note:"))
+        note.allnotes.pop(a)
+        note.showallnotes(self)
+
+    def markAsDone(self):
+        pass
+
+
+a = note()
+a.addnote("this is a test")
+a.addnote("hello")
+a.addnote("this is a another text just to test thigs")
+a.addnote("hello from the other side")
+a.addnote("yolo")
+a.delNote()
+# print(a.allnotes)
+
+
+# alist = [1, 2, 3]
+# for i in alist:
+#     print(f"{i}\n")
