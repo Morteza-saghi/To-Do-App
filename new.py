9c1a02f04d0397786dbd9f19cb54ddc8c5c80aa9
Mo <mort.saghi@gmail.com>
2023-06-24 19:38:23 +0330

    did the mark done func in this branch and now its done

--- new.py
+++ new.py
@@ -16,12 +16,11 @@ class note:
             input("chose the note you wnat to delete\nEnter the Index of the note: "))
         note.allnotes.pop(a)
 
-    def  markAsDone(self):
+    def markAsDone(self):
         note.showallnotes(self)
-        a=int(input("Which have you done: ")) 
-        note.allnotes[a] ="\u0336".join(note.allnotes[a])
+        a = int(input("Which have you done: "))
+        note.allnotes[a] = "\u0336".join(note.allnotes[a])
         note.showallnotes(self)
-
 
 
 a = note()
@@ -32,14 +31,3 @@ a.addnote("go to gym")
 a.markAsDone()
 a.markAsDone()
 a.markAsDone()
-
-
-
-# all the func i have now works just fine
-# SO to test the branching and other things in git
-# lets make a new branch to develop rest of the project
-
-
-
-
-# this is where i wnat to add the markDone function
